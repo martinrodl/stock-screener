54cf944e4da99a108dc3b228d9f98aecc5b50da2
Martin Rodl <axsis@centrum.cz>
2023-12-30 12:02:15 +0100

    Automatic update of yaml

--- update_versions.sh
+++ update_versions.sh
@@ -2,14 +2,16 @@
 
 # Function to update version and commit
 update_version() {
-    cd "$1" || exit
+  cd "$1" || exit
     echo "Updating version in $1"
     npm version patch
-    git add package.json package-lock.json
+    new_version=$(grep '"version":' package.json | cut -d '"' -f 4)
+    kubernetes_yaml_path="../infra/k8s/be-depl.yaml"
+    sed -i '' "s/\(mrodl\/stocks-be:\).*/\1$new_version/" "$kubernetes_yaml_path"
+    git add package.json package-lock.json "$kubernetes_yaml_path"
     cd - || exit
 }
 
-# Path to your frontend and backend directories
 FRONTEND_PATH="./frontend"
 BACKEND_PATH="./be"
 
