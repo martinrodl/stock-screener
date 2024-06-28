794df89d800676340d0020671500d8b9877b7567
Martin Rodl <axsis@centrum.cz>
2024-06-28 16:04:03 +0200

    Added logger into app

--- be-nest/src/main.ts
+++ be-nest/src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log'],
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true })); // whitelist: true will remove any additional properties that are not defined in the DTO
-
+  app.use(morgan('combined'));
   const config = new DocumentBuilder()
     .setTitle('Stock Screener API')
     .setDescription('API for stock screener application')
