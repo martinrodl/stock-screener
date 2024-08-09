1c2e2ebcf1b3089ee5aa43da5919059eaf2ee86f
Martin Rodl <axsis@centrum.cz>
2024-08-09 15:04:24 +0200

    Removed express be

--- update_versions.sh
+++ update_versions.sh
@@ -6,8 +6,8 @@ update_version() {
     echo "Updating version in $1"
     npm version patch
     new_version=$(grep '"version":' package.json | cut -d '"' -f 4)
-    kubernetes_yaml_path="../infra/k8s/be-depl.yaml"
-    sed -i '' "s/\(mrodl\/stocks-be:\).*/\1$new_version/" "$kubernetes_yaml_path"
+    kubernetes_yaml_path="../infra/k8s/be-nest-depl.yaml"
+    sed -i '' "s/\(mrodl\/stocks-be-nest:\).*/\1$new_version/" "$kubernetes_yaml_path"
     git add package.json package-lock.json "$kubernetes_yaml_path"
     cd - || exit
 }
