56c2a8d7ed6c8ee4e894d9e90ada0327b8f05399
Martin Rodl <axsis@centrum.cz>
2024-09-09 20:23:40 +0200

    Fix error missing ActualValues table

--- frontend/src/components/ActualValuesTable.tsx
+++ frontend/src/components/ActualValuesTable.tsx
@@ -28,19 +28,19 @@ const ActualValuesTable = ({ symbol, onLoadComplete }) => {
                     <tr>
                         <td className="py-2 px-4 border-b font-semibold">Date</td>
                         <td className="py-2 px-4 border-b">
-                            {new Date(actualValues.date).toLocaleDateString()}
+                            {new Date(actualValues?.date).toLocaleDateString()}
                         </td>
                     </tr>
                     <tr>
                         <td className="py-2 px-4 border-b font-semibold">CAPE Ratio</td>
-                        <td className="py-2 px-4 border-b">{actualValues.capeRatio.toFixed(2)}</td>
+                        <td className="py-2 px-4 border-b">{actualValues?.capeRatio.toFixed(2)}</td>
                     </tr>
                     <tr>
                         <td className="py-2 px-4 border-b font-semibold">
                             Intrinsic Value (Zero Growth)
                         </td>
                         <td className="py-2 px-4 border-b">
-                            {actualValues.intrinsicValueZeroGrowth.toFixed(2)}
+                            {actualValues?.intrinsicValueZeroGrowth.toFixed(2)}
                         </td>
                     </tr>
                     <tr>
@@ -48,7 +48,7 @@ const ActualValuesTable = ({ symbol, onLoadComplete }) => {
                             Intrinsic Value (Average Growth 5y)
                         </td>
                         <td className="py-2 px-4 border-b">
-                            {actualValues.intrinsicValueAverageGrowth5y.toFixed(2)}
+                            {actualValues?.intrinsicValueAverageGrowth5y.toFixed(2)}
                         </td>
                     </tr>
                     <tr>
@@ -56,19 +56,19 @@ const ActualValuesTable = ({ symbol, onLoadComplete }) => {
                             Intrinsic Value (Last Year Growth)
                         </td>
                         <td className="py-2 px-4 border-b">
-                            {actualValues.intrinsicValueLastYearGrowth.toFixed(2)}
+                            {actualValues?.intrinsicValueLastYearGrowth.toFixed(2)}
                         </td>
                     </tr>
                     <tr>
                         <td className="py-2 px-4 border-b font-semibold">Peter Lynch Value</td>
                         <td className="py-2 px-4 border-b">
-                            {actualValues.peterlynchValue.toFixed(2)}
+                            {actualValues?.peterlynchValue.toFixed(2)}
                         </td>
                     </tr>
                     <tr>
                         <td className="py-2 px-4 border-b font-semibold">Shares Outstanding</td>
                         <td className="py-2 px-4 border-b">
-                            {actualValues.sharesOutstanding.toLocaleString()}
+                            {actualValues?.sharesOutstanding.toLocaleString()}
                         </td>
                     </tr>
                     <tr>
@@ -76,19 +76,19 @@ const ActualValuesTable = ({ symbol, onLoadComplete }) => {
                             Shares Outstanding (5y)
                         </td>
                         <td className="py-2 px-4 border-b">
-                            {actualValues.sharesOutstanding5y.toLocaleString()}
+                            {actualValues?.sharesOutstanding5y.toLocaleString()}
                         </td>
                     </tr>
                     <tr>
                         <td className="py-2 px-4 border-b font-semibold">ROE (5y)</td>
                         <td className="py-2 px-4 border-b">
-                            {(actualValues.roe5y * 100).toFixed(2)}%
+                            {(actualValues?.roe5y * 100).toFixed(2)}%
                         </td>
                     </tr>
                     <tr>
                         <td className="py-2 px-4 border-b font-semibold">ROIC (5y)</td>
                         <td className="py-2 px-4 border-b">
-                            {(actualValues.roic5y * 100).toFixed(2)}%
+                            {(actualValues?.roic5y * 100).toFixed(2)}%
                         </td>
                     </tr>
                     <tr>
@@ -96,7 +96,7 @@ const ActualValuesTable = ({ symbol, onLoadComplete }) => {
                             Average Profit Growth (5y)
                         </td>
                         <td className="py-2 px-4 border-b">
-                            {(actualValues.averageProfitGrowth5y * 100).toFixed(2)}%
+                            {(actualValues?.averageProfitGrowth5y * 100).toFixed(2)}%
                         </td>
                     </tr>
                     <tr>
@@ -104,7 +104,7 @@ const ActualValuesTable = ({ symbol, onLoadComplete }) => {
                             Average Dividend Growth (5y)
                         </td>
                         <td className="py-2 px-4 border-b">
-                            {(actualValues.averageDividendGrowth5y * 100).toFixed(2)}%
+                            {(actualValues?.averageDividendGrowth5y * 100).toFixed(2)}%
                         </td>
                     </tr>
                     <tr>
@@ -112,7 +112,7 @@ const ActualValuesTable = ({ symbol, onLoadComplete }) => {
                             Average Net Income Growth (5y)
                         </td>
                         <td className="py-2 px-4 border-b">
-                            {(actualValues.averageNetIncomeGrowth5y * 100).toFixed(2)}%
+                            {(actualValues?.averageNetIncomeGrowth5y * 100).toFixed(2)}%
                         </td>
                     </tr>
                     <tr>
@@ -120,19 +120,19 @@ const ActualValuesTable = ({ symbol, onLoadComplete }) => {
                             Average Profit Margin (5y)
                         </td>
                         <td className="py-2 px-4 border-b">
-                            {(actualValues.averageProfitMargin5y * 100).toFixed(2)}%
+                            {(actualValues?.averageProfitMargin5y * 100).toFixed(2)}%
                         </td>
                     </tr>
                     <tr>
                         <td className="py-2 px-4 border-b font-semibold">Debt Per Share</td>
                         <td className="py-2 px-4 border-b">
-                            {actualValues.debtPerShare.toFixed(2)}
+                            {actualValues?.debtPerShare.toFixed(2)}
                         </td>
                     </tr>
                     <tr>
                         <td className="py-2 px-4 border-b font-semibold">Dividend Yield (5y)</td>
                         <td className="py-2 px-4 border-b">
-                            {(actualValues.dividendYield5y * 100).toFixed(2)}%
+                            {(actualValues?.dividendYield5y * 100).toFixed(2)}%
                         </td>
                     </tr>
                     <tr>
@@ -140,19 +140,19 @@ const ActualValuesTable = ({ symbol, onLoadComplete }) => {
                             Dividend Payout Ratio (5y)
                         </td>
                         <td className="py-2 px-4 border-b">
-                            {(actualValues.dividendPayoutRatio5y * 100).toFixed(2)}%
+                            {(actualValues?.dividendPayoutRatio5y * 100).toFixed(2)}%
                         </td>
                     </tr>
                     <tr>
                         <td className="py-2 px-4 border-b font-semibold">Buyback Yield</td>
                         <td className="py-2 px-4 border-b">
-                            {(actualValues.buybackYield * 100).toFixed(2)}%
+                            {(actualValues?.buybackYield * 100).toFixed(2)}%
                         </td>
                     </tr>
                     <tr>
                         <td className="py-2 px-4 border-b font-semibold">Free Cash Flow</td>
                         <td className="py-2 px-4 border-b">
-                            {actualValues.freeCashFlow.toLocaleString()}
+                            {actualValues?.freeCashFlow.toLocaleString()}
                         </td>
                     </tr>
                 </tbody>
